fed7e5e23e365ff4ee7b41a0aa3437af9d0d7abe
vindennt <truong.dennis00@gmail.com>
2026-02-07 17:20:26 -0800

    migrated old boilerplate endpoints

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,31 +5,13 @@ import (
 
 	"github.com/vindennt/akasha-showdown-engine/internal/auth"
 	"github.com/vindennt/akasha-showdown-engine/internal/config"
+	"github.com/vindennt/akasha-showdown-engine/internal/middleware"
 )
 
-// CORS middleware
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://localhost:5173")
-		// w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func RegisterRoutes(mux *http.ServeMux, cfg *config.Config) {
 	authClient := auth.NewClient(cfg)
 	itemHandler := NewItemHandler(cfg)
-	// TODO:
-	// logHandler := middleware.Logging(corsHandler)
 	
 	// Health Check
 	mux.HandleFunc("/health/ping", func(w http.ResponseWriter, r *http.Request) {
@@ -37,13 +19,12 @@ func RegisterRoutes(mux *http.ServeMux, cfg *config.Config) {
 		w.Write([]byte(`{"message": "pong"}`))
 	})
 
-	mux.Handle("/auth/signup", corsMiddleware(http.HandlerFunc(authClient.Signup)))
-	mux.Handle("/auth/signin", corsMiddleware(http.HandlerFunc(authClient.Signin)))
+	mux.Handle("/auth/signup", middleware.CORSHandler(http.HandlerFunc(authClient.Signup)))
+	mux.Handle("/auth/signin", middleware.CORSHandler(http.HandlerFunc(authClient.Signin)))
 
-	mux.Handle("/item/create-item", corsMiddleware(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.CreateItem))))
-	mux.Handle("/item/get-item/", corsMiddleware(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.GetItem))))
-	mux.Handle("/item/get-items", corsMiddleware(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.ListItems))))
-	mux.Handle("/item/update-item/", corsMiddleware(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.UpdateItem))))
-	mux.Handle("/item/delete/", corsMiddleware(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.DeleteItem))))
+	mux.Handle("/item/create-item", middleware.CORSHandler(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.CreateItem))))
+	mux.Handle("/item/get-item/", middleware.CORSHandler(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.GetItem))))
+	mux.Handle("/item/get-items", middleware.CORSHandler(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.ListItems))))
+	mux.Handle("/item/update-item/", middleware.CORSHandler(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.UpdateItem))))
+	mux.Handle("/item/delete/", middleware.CORSHandler(authClient.AuthMiddleware(http.HandlerFunc(itemHandler.DeleteItem))))
 }
-
